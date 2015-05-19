a8386d6290f4fe6d8c6b767406d182a514768835
Nizar Ellouze <nizarellouze@yahoo.fr>
2015-05-19 17:16:59 +0100

    clean code

--- src/main/java/org/world/model/CountryLnRepository.java
+++
@@ -1,9 +0,0 @@
-package org.world.model;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "countryln", path = "countryln")
-public interface CountryLnRepository extends PagingAndSortingRepository<CountryLn, Integer> {
-
-}
